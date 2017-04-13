b4225a437e6333ab423254e6220f88a1f4f6ca4a
Anton-Latukha <anton.latukha@gmail.com>
2017-04-14 00:32:10 +0300

    Polish the text of the script.

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ readonly SCRIPT_NAME="$(basename "$0")"
 # Exit codes
 readonly E_RUN_AS_ROOT='1'
 
-## 1. Check if root
+## 1. Check if root privileges available
 if test "$(id -u)" -ne '0'; then
-   echo "ERROR: The \"${SCRIPT_NAME}\" script must be run with root priviliges." 1>&2
+   echo "ERROR: The \"${SCRIPT_NAME}\" script must be run with root privileges" 1>&2
    exit "$E_RUN_AS_ROOT"
 fi
 
@@ -21,7 +21,7 @@ curl --ssl -L https://get.docker.com/ | sh
 ### Enable Docker socket
 systemctl enable docker.socket
 
-### 3.a For Ubuntu 16.04: Temporarely install Docker-Compose as a container (because we going to have ver. >1.6 which is required for Version 2)
+### 3.a For Ubuntu 16.04: Temporarily install Docker-Compose as a container (because we going to have ver. >1.6 which is required for Version 2, and SaltStack)
 curl --ssl -L https://github.com/docker/compose/releases/download/1.8.1/run.sh > /usr/local/bin/docker-compose
 
 #### Do proper rights:
@@ -29,12 +29,13 @@ chown root:docker /usr/local/bin/docker-compose
 chmod u+x /usr/local/bin/docker-compose
 chmod g+x+r /usr/local/bin/docker-compose # Here +r is just to bash can read it
 chmod o-rwx /usr/local/bin/docker-compose
-ls -la /usr/local/bin/docker-compose # -rwxr-x--- 1 root docker *** /usr/local/bin/docker-compose*
+ls -la /usr/local/bin/docker-compose    # -rwxr-x--- 1 root docker *** /usr/local/bin/docker-compose*
 
 ## 4. Install SaltStack
 curl -o /tmp/bootstrap-salt.sh -L https://bootstrap.saltstack.com
 sh /tmp/bootstrap-salt.sh -M -P -A 127.0.0.1 -i MasterA  # Means "also master"
 
+: '
 ### Install SaltStack configuration
 mkdir -p /etc/salt/
 cp --backup=numbered ./salt/master /etc/salt/
@@ -50,23 +51,31 @@ systemctl enable salt-master.service
 systemctl stop salt-minion.service
 systemctl start salt-minion.service
 systemctl enable salt-minion.service
+'
 
-#### Accept key
-RESULT=1
-while test "$RESULT" -ne '0' # Watch for key until it accepted and Zero returned
+### Accept local Minion key
+RETURN=1
+echo 'Please wait for Master and minion to get ready. It can take couple of minutes...'
+while test "$RETURN" -ne '0'    # Watch for key until it accepted and Zero returned
 do
-  salt-key --accept=MasterA --yes
+  echo 'Waiting a Minion key to appear...'
+  salt-key --accept='MasterA' --yes
   salt-key --list accepted | grep MasterA
-  RESULT="$?"
+  RETURN="$?"
 done
 
-#### Wait for Master-Minion get ready
-RESULT=1
-while test "$RESULT" -ne '0' # Wait till Minion runs a commands and Zero returned
+echo 'Key found and accepted.'
+
+### Get Master-Minion ready
+sleep 5    # Wait Minion to get & process acceptance message
+RETURN=1
+while test "$RETURN" -ne '0'    # Wait till Minion runs a command and Zero returned
 do
-  salt --failhard --verbose --timeout=60 'MasterA' test.ping
-  RESULT="$?"
+  salt --failhard --verbose --timeout=240 'MasterA' test.ping
+  RETURN="$?"
 done
 
-## 5. Now deploy the rest of infrastructure with SaltStack
+echo 'Minion is ready.'
+
+## 5. Now everything is ready to deploy the rest of infrastructure with SaltStack
 salt 'MasterA' state.highstate
